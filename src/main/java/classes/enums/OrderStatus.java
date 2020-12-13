7e1ab11c40be306ebc482d0910757a4fba220a77
ListratenkoIgor <listrik_individum@mail.ru>
2020-12-13 17:51:08 +0300

    Feat: add common classes

---
+++ src/main/java/classes/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package classes.enums;
+
+public enum OrderStatus {
+    PROCESSING, READING_ROOM, SUBSCRIPTION
+}
---
+++ src/main/java/classes/enums/UserRole.java
@@ -0,0 +1,5 @@
+package classes.enums;
+
+public enum UserRole {
+    USER, LIBRARIAN
+}
